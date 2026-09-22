66875eeed71c0916e6eee024cf3a04d9010a3910
Alex Page <alex@anpage.me>
2026-09-21 21:39:54 -0400

    Scale cursor closer to the DOS version

--- src/shell/drawmode/overlay_ui.rs
+++ src/shell/drawmode/overlay_ui.rs
@@ -234,8 +234,8 @@ impl OverlayUi {
             if self.show_cursor {
                 let cursor_pos_1 = egui::pos2(mouse_state.pos_x as f32, mouse_state.pos_y as f32);
                 let cursor_pos_2 = egui::pos2(
-                    mouse_state.pos_x as f32 + 29.0 * 1.5,
-                    mouse_state.pos_y as f32 + 25.0 * 1.5,
+                    mouse_state.pos_x as f32 + 29.0 * scale_factor,
+                    mouse_state.pos_y as f32 + 25.0 * scale_factor,
                 );
                 let cursor_image = egui::Shape::image(
                     cursor_texture,
